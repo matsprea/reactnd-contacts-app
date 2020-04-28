2a2aff634e2a47957bfa7e2d38bd0b941d9ad1f5
Matteo Spreafico <matsprea@users.noreply.github.com>
2020-04-28 09:15:27 +0100

    CreateContact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
 class App extends Component {
 
   state = {
-    contacts : []
+    contacts : [],
+    screen: 'list'
   }
 
   componentDidMount() {
@@ -25,14 +27,12 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-        />
-      </div>
-    );
+    return <div>
+        {this.state.screen === 'list' && <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact} onNavigate={() => {
+              this.setState(() => ({ screen: 'create' }));
+            }} />}
+        {this.state.screen === 'create' && <CreateContact />}
+      </div>;
   }
 }
 
---
+++ src/CreateContact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class CreateComponent extends Component {
+    render() {
+        return (
+        <div>Create Contact</div>
+        )
+    }
+}
+
+export default CreateComponent
